405d2438ab2714a2cd4bb9c7887bf6ddc5a94c64
Prem <prem.tomar@goswift.in>
2023-05-08 22:32:25 +0530

    Added logging level debug in local profile Changed Hikari pool configuration

--- src/main/java/com/rapigrow/main/common/config/HikariPoolConfig.java
+++ src/main/java/com/rapigrow/main/common/config/HikariPoolConfig.java
@@ -1,5 +1,6 @@
 package com.rapigrow.main.common.config;
 
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
@@ -7,23 +8,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class HikariConfig {
+public class HikariPoolConfig {
 
     @Autowired
     private ApplicationConfig applicationConfig;
 
     @Bean
     public HikariDataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+
+
         String URL_SEPARATOR = "/";
         var db = applicationConfig.getDb();
         String dbUrl = db.getUrl() + db.getHost() + ":" + db.getPort() + URL_SEPARATOR + db.getName();
-        return DataSourceBuilder
+        config.setDataSource(DataSourceBuilder
                 .create()
                 .type(HikariDataSource.class)
                 .password(db.getPassword())
                 .username(db.getUsername())
                 .url(dbUrl)
-                .build();
+                .build());
+
+        config.setMaximumPoolSize(100);
+        config.setMinimumIdle(10);
+        config.setPoolName("main-db-pool");
+
+        return new HikariDataSource(config);
     }
 
 
