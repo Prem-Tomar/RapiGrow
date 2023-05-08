fd581deafeb2b39054f12508a19002ad4e62ad33
Prem <prem.tomar@goswift.in>
2023-05-08 21:44:45 +0530

    Added db configurations and Application Configurations. changed Hikari configuration to use env variables

---
+++ src/main/java/com/rapigrow/main/common/config/ApplicationConfig.java
@@ -0,0 +1,38 @@
+package com.rapigrow.main.common.config;
+
+import jakarta.annotation.Nonnull;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "rapigrow")
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class ApplicationConfig {
+
+    private DB db;
+    @Getter
+    @Setter
+    @ToString
+    @EqualsAndHashCode
+    public static class DB {
+        @Nonnull
+        private String username;
+        @Nonnull
+        private String password;
+        @Nonnull
+        private String host;
+        @Nonnull
+        private String port;
+        @Nonnull
+        private String name;
+        @Nonnull
+        private String url;
+    }
+}
--- src/main/java/com/rapigrow/main/common/config/HikariConfig.java
+++ src/main/java/com/rapigrow/main/common/config/HikariConfig.java
@@ -1,6 +1,7 @@
 package com.rapigrow.main.common.config;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,10 +9,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class HikariConfig {
 
+    @Autowired
+    private ApplicationConfig applicationConfig;
 
     @Bean
     public HikariDataSource dataSource() {
-        return DataSourceBuilder.create().type(HikariDataSource.class).password("Vishu@894").url("jdbc:mysql://localhost:3306/premproject_api").username("root").build();
+        String URL_SEPARATOR = "/";
+        var db = applicationConfig.getDb();
+        String dbUrl = db.getUrl() + db.getHost() + ":" + db.getPort() + URL_SEPARATOR + db.getName();
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .password(db.getPassword())
+                .username(db.getUsername())
+                .url(dbUrl)
+                .build();
     }
 
 
