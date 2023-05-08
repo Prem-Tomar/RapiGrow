5b7896a8d35bc6f704918aa5b1503297b338dabe
Prem Pratap Singh Tomar <prem.tomar.gwl@gmail.com>
2023-05-09 00:14:05 +0530

    Added exception handler

--- src/main/java/com/rapigrow/main/common/exceptions/handlers/RapiGrowExceptionHandler.java
+++ src/main/java/com/rapigrow/main/common/exceptions/handlers/RapiGrowExceptionHandler.java
@@ -1,7 +1,19 @@
 package com.rapigrow.main.common.exceptions.handlers;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class RapiGrowExceptionHandler {
+public class RapiGrowExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<Object> handleUserUnauthorisedException(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, null, new HttpHeaders(), HttpStatus.UNAUTHORIZED, request);
+    }
+
 }
