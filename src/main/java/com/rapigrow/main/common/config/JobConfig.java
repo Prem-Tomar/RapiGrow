36bdf6627a7a6d25014a2e656bac66df58effe1d
Prem <prem.tomar@goswift.in>
2023-05-09 11:37:25 +0530

    Disabled the job configuration till the time it is needed

--- src/main/java/com/rapigrow/main/common/config/JobConfig.java
+++ src/main/java/com/rapigrow/main/common/config/JobConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Configuration
+//@Configuration
 //@EnableScheduling
 //@EnableAspectJAutoProxy
 public class JobConfig {
