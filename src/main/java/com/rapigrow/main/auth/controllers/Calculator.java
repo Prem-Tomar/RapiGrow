cc8c34779c44c656b73f5b16f0013b81ff5beb3b
Prem Tomar <prem.tomar.gwl@gmail.com>
2023-05-11 15:18:16 +0530

    Refactored authentication package from sec to auth

--- src/main/java/com/rapigrow/main/auth/controllers/Calculator.java
+++ src/main/java/com/rapigrow/main/auth/controllers/Calculator.java
@@ -1,17 +1,14 @@
-package com.rapigrow.main.sec.controllers;
+package com.rapigrow.main.auth.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "calulatefunction")
 public class Calculator {
 
     @GetMapping("/multiply")
-    public int mul(@RequestParam int a, @RequestParam int b) {
-        return a * b;
+    public int mul(@RequestParam int a, @RequestParam int b){
+        return a*b;
     }
 
 //    @PostMapping("post")
@@ -19,28 +16,31 @@ public class Calculator {
 //        return a*b;
 //    }
 
-    @GetMapping("fire")
-    public String check() {
+@GetMapping("fire")
+public String check(){
         return "this is check";
-    }
+}
 
 
     @GetMapping("/Sub")
-    public int sub(@RequestParam int a, @RequestParam int b) {
-        return a - b;
+    public int sub(@RequestParam int a, @RequestParam int b){
+        return a-b;
     }
 
     @GetMapping("/div")
-    public String divide(@RequestParam int a, @RequestParam int b) {
+    public String divide(@RequestParam int a, @RequestParam int b){
 
-        if (b != 0) {
-            float ans = a / b;
-            String s = String.valueOf(ans);
+        if(b!=0){
+            float ans=a/b;
+            String s=String.valueOf(ans);
             return s;
-        } else {
+        }
+        else{
             return "Can Not Divide By Zero";
         }
     }
+
+
 
 
 }
