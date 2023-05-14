187d55393df8714071def50f6c98ca37df062206
Prem <prem.tomar.gwl@gmail.com>
2023-05-15 01:05:46 +0530

    Remove key title from RequestMapping annotation

--- src/main/java/com/rapigrow/main/auth/controllers/AuthController.java
+++ src/main/java/com/rapigrow/main/auth/controllers/AuthController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "auth")
+@RequestMapping("auth")
 public class AuthController {
     @Autowired
     private ObjectMapper objectMapper;
