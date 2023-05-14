baa419c5564152b3443c1b5df17c3f2c5f874ee1
Prem Pratap Singh Tomar <prem.tomar.gwl@gmail.com>
2023-05-14 13:26:08 +0530

    Changed scope to request scope for user model

--- src/main/java/com/rapigrow/main/auth/models/User.java
+++ src/main/java/com/rapigrow/main/auth/models/User.java
@@ -3,10 +3,12 @@ package com.rapigrow.main.auth.models;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.web.context.annotation.RequestScope;
 
 @Getter
 @Setter
 @ToString
+@RequestScope
 public class User {
     private String uid;
     private String email;
